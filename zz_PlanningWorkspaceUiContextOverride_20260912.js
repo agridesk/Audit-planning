f784c86c0fdc04b025e442bea1a8a4e81d5af835
agridesk <rombouts@agridesk.com>
2026-09-13 16:21:41 +0200

    Workspace: keep planned audit actions in late UI override

--- zz_PlanningWorkspaceUiContextOverride_20260912.js
+++ zz_PlanningWorkspaceUiContextOverride_20260912.js
@@ -1,8 +1,8 @@
 /***********************************************************************
  * zz_PlanningWorkspaceUiContextOverride_20260912.js
- * BUILD: 2026-09-12_PLANNING_WORKSPACE_UI_R9_POINTER_DRAG_FALLBACK
+ * BUILD: 2026-09-13_PLANNING_WORKSPACE_UI_R10_PLANNED_ACTIONS_OVERRIDE
  ***********************************************************************/
-var PLANNING_WORKSPACE_UI_RENDERER_BUILD='2026-09-12_PLANNING_WORKSPACE_UI_R9_POINTER_DRAG_FALLBACK';
+var PLANNING_WORKSPACE_UI_RENDERER_BUILD='2026-09-13_PLANNING_WORKSPACE_UI_R10_PLANNED_ACTIONS_OVERRIDE';
 
 function PlanningWorkspaceUi_render(ctx){
   ctx=ctx||{};
@@ -16,7 +16,8 @@ function PlanningWorkspaceUi_render(ctx){
   var contextEnhancer=HtmlService.createHtmlOutputFromFile('PlanningWorkspaceContextEnhancer.js').getContent();
   var attentionEnhancer=HtmlService.createHtmlOutputFromFile('PlanningWorkspaceAttentionEnhancer.js').getContent();
   var pointerDragFallback=HtmlService.createHtmlOutputFromFile('PlanningWorkspacePointerDragFallback.js').getContent();
-  html=html.replace('</body>',dragDrop+'\n'+detailToolkit+'\n'+contextEnhancer+'\n'+attentionEnhancer+'\n'+pointerDragFallback+'\n</body>');
+  var plannedActions=HtmlService.createHtmlOutputFromFile('PlanningWorkspacePlannedAuditActions.js').getContent();
+  html=html.replace('</body>',dragDrop+'\n'+detailToolkit+'\n'+contextEnhancer+'\n'+attentionEnhancer+'\n'+pointerDragFallback+'\n'+plannedActions+'\n</body>');
   return HtmlService.createHtmlOutput(html).setTitle('AMS - Planning Workspace');
 }
 
@@ -29,6 +30,7 @@ function PlanningWorkspaceUi_contract(){return{
   contextEnhancerInclude:'PlanningWorkspaceContextEnhancer.js',
   attentionEnhancerInclude:'PlanningWorkspaceAttentionEnhancer.js',
   pointerDragFallbackInclude:'PlanningWorkspacePointerDragFallback.js',
+  plannedActionsInclude:'PlanningWorkspacePlannedAuditActions.js',
   evaluatedTemplate:true,
   serverSeed:false,
   dataIndependentShell:true,
@@ -38,6 +40,8 @@ function PlanningWorkspaceUi_contract(){return{
   contextEnhancerClientOnly:true,
   attentionEnhancerClientOnly:true,
   pointerDragFallbackClientOnly:true,
+  plannedActionsClientOnly:true,
+  plannedDetailOnDemand:true,
   attentionPriority:'planningWindowTo, planningWindowFrom, blocker state',
   directSheetReads:false,
   directSheetWrites:false,

2d722dbef7568f276295e0166d2f40d443a6eeb3
agridesk <rombouts@agridesk.com>
2026-09-13 16:40:12 +0200

    Include concept review age UX in Planning Workspace

--- zz_PlanningWorkspaceUiContextOverride_20260912.js
+++ zz_PlanningWorkspaceUiContextOverride_20260912.js
@@ -1,8 +1,8 @@
 /***********************************************************************
  * zz_PlanningWorkspaceUiContextOverride_20260912.js
- * BUILD: 2026-09-13_PLANNING_WORKSPACE_UI_R10_PLANNED_ACTIONS_OVERRIDE
+ * BUILD: 2026-09-13_PLANNING_WORKSPACE_UI_R11_CONCEPT_REVIEW
  ***********************************************************************/
-var PLANNING_WORKSPACE_UI_RENDERER_BUILD='2026-09-13_PLANNING_WORKSPACE_UI_R10_PLANNED_ACTIONS_OVERRIDE';
+var PLANNING_WORKSPACE_UI_RENDERER_BUILD='2026-09-13_PLANNING_WORKSPACE_UI_R11_CONCEPT_REVIEW';
 
 function PlanningWorkspaceUi_render(ctx){
   ctx=ctx||{};
@@ -17,7 +17,8 @@ function PlanningWorkspaceUi_render(ctx){
   var attentionEnhancer=HtmlService.createHtmlOutputFromFile('PlanningWorkspaceAttentionEnhancer.js').getContent();
   var pointerDragFallback=HtmlService.createHtmlOutputFromFile('PlanningWorkspacePointerDragFallback.js').getContent();
   var plannedActions=HtmlService.createHtmlOutputFromFile('PlanningWorkspacePlannedAuditActions.js').getContent();
-  html=html.replace('</body>',dragDrop+'\n'+detailToolkit+'\n'+contextEnhancer+'\n'+attentionEnhancer+'\n'+pointerDragFallback+'\n'+plannedActions+'\n</body>');
+  var conceptReview=HtmlService.createHtmlOutputFromFile('PlanningWorkspaceConceptReview.js').getContent();
+  html=html.replace('</body>',dragDrop+'\n'+detailToolkit+'\n'+contextEnhancer+'\n'+attentionEnhancer+'\n'+pointerDragFallback+'\n'+plannedActions+'\n'+conceptReview+'\n</body>');
   return HtmlService.createHtmlOutput(html).setTitle('AMS - Planning Workspace');
 }
 
@@ -31,6 +32,7 @@ function PlanningWorkspaceUi_contract(){return{
   attentionEnhancerInclude:'PlanningWorkspaceAttentionEnhancer.js',
   pointerDragFallbackInclude:'PlanningWorkspacePointerDragFallback.js',
   plannedActionsInclude:'PlanningWorkspacePlannedAuditActions.js',
+  conceptReviewInclude:'PlanningWorkspaceConceptReview.js',
   evaluatedTemplate:true,
   serverSeed:false,
   dataIndependentShell:true,
@@ -41,7 +43,11 @@ function PlanningWorkspaceUi_contract(){return{
   attentionEnhancerClientOnly:true,
   pointerDragFallbackClientOnly:true,
   plannedActionsClientOnly:true,
+  conceptReviewClientOnly:true,
   plannedDetailOnDemand:true,
+  conceptReviewUsesLoadedReservationMetadata:true,
+  conceptReviewExtraRpcs:0,
+  conceptReviewThresholdMonths:6,
   attentionPriority:'planningWindowTo, planningWindowFrom, blocker state',
   directSheetReads:false,
   directSheetWrites:false,

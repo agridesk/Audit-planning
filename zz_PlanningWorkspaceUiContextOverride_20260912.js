c45278a4a9abf605c1d94b7422cb17398bc58263
agridesk <rombouts@agridesk.com>
2026-09-13 17:05:44 +0200

    Include planned status color enhancer in Workspace

--- zz_PlanningWorkspaceUiContextOverride_20260912.js
+++ zz_PlanningWorkspaceUiContextOverride_20260912.js
@@ -1,8 +1,8 @@
 /***********************************************************************
  * zz_PlanningWorkspaceUiContextOverride_20260912.js
- * BUILD: 2026-09-13_PLANNING_WORKSPACE_UI_R11_CONCEPT_REVIEW
+ * BUILD: 2026-09-13_PLANNING_WORKSPACE_UI_R12_STATUS_VISUALS
  ***********************************************************************/
-var PLANNING_WORKSPACE_UI_RENDERER_BUILD='2026-09-13_PLANNING_WORKSPACE_UI_R11_CONCEPT_REVIEW';
+var PLANNING_WORKSPACE_UI_RENDERER_BUILD='2026-09-13_PLANNING_WORKSPACE_UI_R12_STATUS_VISUALS';
 
 function PlanningWorkspaceUi_render(ctx){
   ctx=ctx||{};
@@ -17,8 +17,9 @@ function PlanningWorkspaceUi_render(ctx){
   var attentionEnhancer=HtmlService.createHtmlOutputFromFile('PlanningWorkspaceAttentionEnhancer.js').getContent();
   var pointerDragFallback=HtmlService.createHtmlOutputFromFile('PlanningWorkspacePointerDragFallback.js').getContent();
   var plannedActions=HtmlService.createHtmlOutputFromFile('PlanningWorkspacePlannedAuditActions.js').getContent();
+  var plannedStatusVisuals=HtmlService.createHtmlOutputFromFile('PlanningWorkspacePlannedStatusVisuals.js').getContent();
   var conceptReview=HtmlService.createHtmlOutputFromFile('PlanningWorkspaceConceptReview.js').getContent();
-  html=html.replace('</body>',dragDrop+'\n'+detailToolkit+'\n'+contextEnhancer+'\n'+attentionEnhancer+'\n'+pointerDragFallback+'\n'+plannedActions+'\n'+conceptReview+'\n</body>');
+  html=html.replace('</body>',dragDrop+'\n'+detailToolkit+'\n'+contextEnhancer+'\n'+attentionEnhancer+'\n'+pointerDragFallback+'\n'+plannedActions+'\n'+plannedStatusVisuals+'\n'+conceptReview+'\n</body>');
   return HtmlService.createHtmlOutput(html).setTitle('AMS - Planning Workspace');
 }
 
@@ -32,27 +33,10 @@ function PlanningWorkspaceUi_contract(){return{
   attentionEnhancerInclude:'PlanningWorkspaceAttentionEnhancer.js',
   pointerDragFallbackInclude:'PlanningWorkspacePointerDragFallback.js',
   plannedActionsInclude:'PlanningWorkspacePlannedAuditActions.js',
+  plannedStatusVisualsInclude:'PlanningWorkspacePlannedStatusVisuals.js',
   conceptReviewInclude:'PlanningWorkspaceConceptReview.js',
-  evaluatedTemplate:true,
-  serverSeed:false,
-  dataIndependentShell:true,
-  decisionDataDeferred:true,
-  dragDropClientOnly:true,
-  detailToolkitClientOnly:true,
-  contextEnhancerClientOnly:true,
-  attentionEnhancerClientOnly:true,
-  pointerDragFallbackClientOnly:true,
-  plannedActionsClientOnly:true,
-  conceptReviewClientOnly:true,
-  plannedDetailOnDemand:true,
-  conceptReviewUsesLoadedReservationMetadata:true,
-  conceptReviewExtraRpcs:0,
-  conceptReviewThresholdMonths:6,
-  attentionPriority:'planningWindowTo, planningWindowFrom, blocker state',
-  directSheetReads:false,
-  directSheetWrites:false,
-  planningServiceReadsDuringRender:false,
-  contextExtraReads:0,
-  contextExtraRpcs:0,
-  newSsot:false
+  evaluatedTemplate:true,serverSeed:false,dataIndependentShell:true,decisionDataDeferred:true,
+  dragDropClientOnly:true,detailToolkitClientOnly:true,contextEnhancerClientOnly:true,attentionEnhancerClientOnly:true,pointerDragFallbackClientOnly:true,plannedActionsClientOnly:true,plannedStatusVisualsClientOnly:true,conceptReviewClientOnly:true,
+  plannedDetailOnDemand:true,conceptReviewUsesLoadedReservationMetadata:true,conceptReviewExtraRpcs:0,conceptReviewThresholdMonths:6,
+  attentionPriority:'planningWindowTo, planningWindowFrom, blocker state',directSheetReads:false,directSheetWrites:false,planningServiceReadsDuringRender:false,contextExtraReads:0,contextExtraRpcs:0,newSsot:false
 };}

d4265041c56faf0aa14629d4e38b72595fb7e982
agridesk <rombouts@agridesk.com>
2026-09-15 17:19:36 +0200

    Include planned state deduper in Planning Workspace

--- zz_PlanningWorkspaceUiContextOverride_20260912.js
+++ zz_PlanningWorkspaceUiContextOverride_20260912.js
@@ -1,8 +1,8 @@
 /***********************************************************************
  * zz_PlanningWorkspaceUiContextOverride_20260912.js
- * BUILD: 2026-09-14_PLANNING_WORKSPACE_UI_R15_MODIFY_WINDOW_GUARD
+ * BUILD: 2026-09-15_PLANNING_WORKSPACE_UI_R16_PLANNED_STATE_DEDUPER
  ***********************************************************************/
-var PLANNING_WORKSPACE_UI_RENDERER_BUILD='2026-09-14_PLANNING_WORKSPACE_UI_R15_MODIFY_WINDOW_GUARD';
+var PLANNING_WORKSPACE_UI_RENDERER_BUILD='2026-09-15_PLANNING_WORKSPACE_UI_R16_PLANNED_STATE_DEDUPER';
 
 function PlanningWorkspaceUi_render(ctx){
   ctx=ctx||{};
@@ -18,10 +18,11 @@ function PlanningWorkspaceUi_render(ctx){
   var pointerDragFallback=HtmlService.createHtmlOutputFromFile('PlanningWorkspacePointerDragFallback.js').getContent();
   var plannedActions=HtmlService.createHtmlOutputFromFile('PlanningWorkspacePlannedAuditActions.js').getContent();
   var plannedStatusVisuals=HtmlService.createHtmlOutputFromFile('PlanningWorkspacePlannedStatusVisuals.js').getContent();
+  var plannedStateDeduper=HtmlService.createHtmlOutputFromFile('PlanningWorkspacePlannedStateDeduper.js').getContent();
   var conceptReview=HtmlService.createHtmlOutputFromFile('PlanningWorkspaceConceptReview.js').getContent();
   var uxStabilizer=HtmlService.createHtmlOutputFromFile('PlanningWorkspaceUxStabilizer.js').getContent();
   var modifyWindowGuard=HtmlService.createHtmlOutputFromFile('PlanningWorkspaceModifyWindowGuard.js').getContent();
-  html=html.replace('</body>',dragDrop+'\n'+detailToolkit+'\n'+contextEnhancer+'\n'+attentionEnhancer+'\n'+pointerDragFallback+'\n'+plannedActions+'\n'+plannedStatusVisuals+'\n'+conceptReview+'\n'+uxStabilizer+'\n'+modifyWindowGuard+'\n</body>');
+  html=html.replace('</body>',dragDrop+'\n'+detailToolkit+'\n'+contextEnhancer+'\n'+attentionEnhancer+'\n'+pointerDragFallback+'\n'+plannedActions+'\n'+plannedStatusVisuals+'\n'+plannedStateDeduper+'\n'+conceptReview+'\n'+uxStabilizer+'\n'+modifyWindowGuard+'\n</body>');
   return HtmlService.createHtmlOutput(html).setTitle('AMS - Planning Workspace');
 }
 
@@ -36,12 +37,13 @@ function PlanningWorkspaceUi_contract(){return{
   pointerDragFallbackInclude:'PlanningWorkspacePointerDragFallback.js',
   plannedActionsInclude:'PlanningWorkspacePlannedAuditActions.js',
   plannedStatusVisualsInclude:'PlanningWorkspacePlannedStatusVisuals.js',
+  plannedStateDeduperInclude:'PlanningWorkspacePlannedStateDeduper.js',
   conceptReviewInclude:'PlanningWorkspaceConceptReview.js',
   uxStabilizerInclude:'PlanningWorkspaceUxStabilizer.js',
   modifyWindowGuardInclude:'PlanningWorkspaceModifyWindowGuard.js',
   modifyWindowGuardRequired:true,
   evaluatedTemplate:true,serverSeed:false,dataIndependentShell:true,decisionDataDeferred:true,
-  dragDropClientOnly:true,detailToolkitClientOnly:true,contextEnhancerClientOnly:true,attentionEnhancerClientOnly:true,pointerDragFallbackClientOnly:true,plannedActionsClientOnly:true,plannedStatusVisualsClientOnly:true,conceptReviewClientOnly:true,uxStabilizerClientOnly:true,modifyWindowGuardClientOnly:true,
+  dragDropClientOnly:true,detailToolkitClientOnly:true,contextEnhancerClientOnly:true,attentionEnhancerClientOnly:true,pointerDragFallbackClientOnly:true,plannedActionsClientOnly:true,plannedStatusVisualsClientOnly:true,plannedStateDeduperClientOnly:true,conceptReviewClientOnly:true,uxStabilizerClientOnly:true,modifyWindowGuardClientOnly:true,
   plannedDetailOnDemand:true,conceptReviewUsesLoadedReservationMetadata:true,conceptReviewExtraRpcs:0,conceptReviewThresholdMonths:6,
   fastWeekNavigationClientOnly:true,fastMonthNavigationClientOnly:true,navigationExtraRpcs:0,modalInteractionGuard:true,
   navigationObserverScope:'calendar root only / body direct children only',navigationMutationStormRemoved:true,plannedStatusObserverDebounced:true,conceptReviewObserverDebounced:true,

2348bb16c87ae7d4eaca41a01543eb3da3751093
agridesk <rombouts@agridesk.com>
2026-09-16 19:52:05 +0200

    Fix preflight flow test to assert callback gating semantics

--- PlanningWorkspaceBatchCommitUiTests.js
+++ PlanningWorkspaceBatchCommitUiTests.js
@@ -1,20 +1,22 @@
 /***********************************************************************
  * PlanningWorkspaceBatchCommitUiTests.js
- * BUILD: 2026-09-16_ROADMAP_2_4_WORKSPACE_BATCH_COMMIT_UI_TESTS_R3_PREFLIGHT_FLOW_ASSERTION
+ * BUILD: 2026-09-16_ROADMAP_2_4_WORKSPACE_BATCH_COMMIT_UI_TESTS_R4_CALLBACK_FLOW
  * Non-destructive source/wiring regression.
  ***********************************************************************/
-var PLANNING_WORKSPACE_BATCH_COMMIT_UI_TEST_BUILD='2026-09-16_ROADMAP_2_4_WORKSPACE_BATCH_COMMIT_UI_TESTS_R3_PREFLIGHT_FLOW_ASSERTION';
+var PLANNING_WORKSPACE_BATCH_COMMIT_UI_TEST_BUILD='2026-09-16_ROADMAP_2_4_WORKSPACE_BATCH_COMMIT_UI_TESTS_R4_CALLBACK_FLOW';
 function RUN_PLANNING_WORKSPACE_BATCH_COMMIT_UI_REGRESSION(){
  var r=[];function t(n,o,d){r.push({name:n,ok:!!o,detail:o?'':String(d||'failed')});}
  var html=HtmlService.createHtmlOutputFromFile('PlanningWorkspace').getContent();
  var js=HtmlService.createHtmlOutputFromFile('PlanningWorkspaceBatchCommit.js').getContent();
- var commitFn=js.indexOf('function commit()'),preflightCall=js.indexOf('PlanningWorkspaceRpc_batchPreflight',commitFn),doCommitCall=js.indexOf('doCommit(rows,b,wall)',commitFn),doCommitFn=js.indexOf('function doCommit('),batchCommitCall=js.indexOf('PlanningWorkspaceRpc_batchCommit',doCommitFn);
+ var commitFn=js.indexOf('function commit()'),preflightCall=js.indexOf('PlanningWorkspaceRpc_batchPreflight',commitFn),doCommitCall=js.indexOf('doCommit(rows,b,wall)',commitFn),allReadyGuard=js.indexOf('if(d.allReady!==true)',commitFn),guardReturn=js.indexOf('return;',allReadyGuard),doCommitFn=js.indexOf('function doCommit('),batchCommitCall=js.indexOf('PlanningWorkspaceRpc_batchCommit',doCommitFn);
  t('workspaceLoadsEnhancer',html.indexOf("PlanningWorkspaceBatchCommit.js")>=0);
  t('enhancerBuild',js.indexOf('WORKSPACE_BATCH_COMMIT_UI_R2_PREFLIGHT_FIRST')>=0);
  t('preflightRpcUsed',preflightCall>=0);
  t('batchRpcUsed',batchCommitCall>=0);
- t('preflightControlsCommit',commitFn>=0&&preflightCall>commitFn&&doCommitCall>preflightCall&&doCommitFn>=0&&batchCommitCall>doCommitFn,'commitFn='+commitFn+' preflight='+preflightCall+' doCommitCall='+doCommitCall+' doCommitFn='+doCommitFn+' batchCommit='+batchCommitCall);
- t('allReadyRequired',js.indexOf('d.allReady!==true',preflightCall)>=0);
+ t('allReadyRequired',allReadyGuard>commitFn);
+ t('blockedReturnsBeforeDoCommit',guardReturn>allReadyGuard&&doCommitCall>guardReturn,'guard='+allReadyGuard+' return='+guardReturn+' doCommitCall='+doCommitCall);
+ t('preflightSuccessCallsCommit',doCommitCall>allReadyGuard&&doCommitCall<preflightCall,'guard='+allReadyGuard+' doCommitCall='+doCommitCall+' preflightInvocation='+preflightCall);
+ t('canonicalCommitIsSeparatedHelper',doCommitFn>=0&&batchCommitCall>doCommitFn);
  t('blockedStopsCommit',js.indexOf("Preview: ")>=0&&js.indexOf('blockedSummary')>=0);
  t('selectedStagedOnly',js.indexOf('selectedStaged')>=0&&js.indexOf('reservations()')>=0);
  t('bounded20',js.indexOf('rows.length>20')>=0);

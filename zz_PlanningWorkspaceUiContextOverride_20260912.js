cf1a708376b2aed4aae718195de6c2207dcd5eef
agridesk <rombouts@agridesk.com>
2026-09-17 13:07:31 +0200

    Wire rotation preassignment warning into Planning Workspace

--- zz_PlanningWorkspaceUiContextOverride_20260912.js
+++ zz_PlanningWorkspaceUiContextOverride_20260912.js
@@ -1,8 +1,8 @@
 /***********************************************************************
  * zz_PlanningWorkspaceUiContextOverride_20260912.js
- * BUILD: 2026-09-16_PLANNING_WORKSPACE_UI_R19_SELECTED_DAY
+ * BUILD: 2026-09-17_PLANNING_WORKSPACE_UI_R20_ROTATION_PREASSIGN_GUARD
  ***********************************************************************/
-var PLANNING_WORKSPACE_UI_RENDERER_BUILD='2026-09-16_PLANNING_WORKSPACE_UI_R19_SELECTED_DAY';
+var PLANNING_WORKSPACE_UI_RENDERER_BUILD='2026-09-17_PLANNING_WORKSPACE_UI_R20_ROTATION_PREASSIGN_GUARD';
 
 function PlanningWorkspaceUi_render(ctx){
   ctx=ctx||{};
@@ -24,7 +24,8 @@ function PlanningWorkspaceUi_render(ctx){
   var uxStabilizer=HtmlService.createHtmlOutputFromFile('PlanningWorkspaceUxStabilizer.js').getContent();
   var modifyWindowGuard=HtmlService.createHtmlOutputFromFile('PlanningWorkspaceModifyWindowGuard.js').getContent();
   var modifyPickerWeekColumns=HtmlService.createHtmlOutputFromFile('PlanningWorkspaceModifyPickerWeekColumns.js').getContent();
-  html=html.replace('</body>',dragDrop+'\n'+detailToolkit+'\n'+contextEnhancer+'\n'+attentionEnhancer+'\n'+pointerDragFallback+'\n'+plannedActions+'\n'+plannedStatusVisuals+'\n'+plannedStateDeduper+'\n'+availabilityLabels+'\n'+conceptReview+'\n'+uxStabilizer+'\n'+modifyWindowGuard+'\n'+modifyPickerWeekColumns+'\n</body>');
+  var rotationPreassignGuard=HtmlService.createHtmlOutputFromFile('PlanningWorkspaceRotationPreassignGuard.js').getContent();
+  html=html.replace('</body>',dragDrop+'\n'+detailToolkit+'\n'+contextEnhancer+'\n'+attentionEnhancer+'\n'+pointerDragFallback+'\n'+plannedActions+'\n'+plannedStatusVisuals+'\n'+plannedStateDeduper+'\n'+availabilityLabels+'\n'+conceptReview+'\n'+uxStabilizer+'\n'+modifyWindowGuard+'\n'+modifyPickerWeekColumns+'\n'+rotationPreassignGuard+'\n</body>');
   return HtmlService.createHtmlOutput(html).setTitle('AMS - Planning Workspace');
 }
 
@@ -45,12 +46,14 @@ function PlanningWorkspaceUi_contract(){return{
   uxStabilizerInclude:'PlanningWorkspaceUxStabilizer.js',
   modifyWindowGuardInclude:'PlanningWorkspaceModifyWindowGuard.js',
   modifyPickerWeekColumnsInclude:'PlanningWorkspaceModifyPickerWeekColumns.js',
+  rotationPreassignGuardInclude:'PlanningWorkspaceRotationPreassignGuard.js',
+  rotationPreassignGuardRequired:true,
   modifyWindowGuardRequired:true,
   evaluatedTemplate:true,serverSeed:false,dataIndependentShell:true,decisionDataDeferred:true,
-  dragDropClientOnly:true,detailToolkitClientOnly:true,contextEnhancerClientOnly:true,attentionEnhancerClientOnly:true,pointerDragFallbackClientOnly:true,plannedActionsClientOnly:true,plannedStatusVisualsClientOnly:true,plannedStateDeduperClientOnly:true,availabilityLabelsClientOnly:true,conceptReviewClientOnly:true,uxStabilizerClientOnly:true,modifyWindowGuardClientOnly:true,modifyPickerWeekColumnsClientOnly:true,
+  dragDropClientOnly:true,detailToolkitClientOnly:true,contextEnhancerClientOnly:true,attentionEnhancerClientOnly:true,pointerDragFallbackClientOnly:true,plannedActionsClientOnly:true,plannedStatusVisualsClientOnly:true,plannedStateDeduperClientOnly:true,availabilityLabelsClientOnly:true,conceptReviewClientOnly:true,uxStabilizerClientOnly:true,modifyWindowGuardClientOnly:true,modifyPickerWeekColumnsClientOnly:true,rotationPreassignGuardClientOnly:true,
   plannedDetailOnDemand:true,conceptReviewUsesLoadedReservationMetadata:true,conceptReviewExtraRpcs:0,conceptReviewThresholdMonths:6,
   fastWeekNavigationClientOnly:true,fastMonthNavigationClientOnly:true,navigationExtraRpcs:0,modalInteractionGuard:true,
   navigationObserverScope:'calendar root only / body direct children only',navigationMutationStormRemoved:true,plannedStatusObserverDebounced:true,conceptReviewObserverDebounced:true,
   planningWindowVisibleOnModify:true,availabilityLabelsUnified:true,technicalAvailabilityStatusesHidden:true,fixedModifyWeekdayColumns:true,modifyPickerSelectedDayFeedback:true,modifyPickerSelectionReconcilesClientSide:true,modifyPickerWeekColumnsExtraRpcs:0,
-  attentionPriority:'planningWindowTo, planningWindowFrom, blocker state',directSheetReads:false,directSheetWrites:false,planningServiceReadsDuringRender:false,contextExtraReads:0,contextExtraRpcs:0,newSsot:false
+  attentionPriority:'planningWindowTo, planningWindowFrom, blocker state',rotationPreassignWarningVisible:true,rotationCanonicalRevalidationRequired:true,directSheetReads:false,directSheetWrites:false,planningServiceReadsDuringRender:false,contextExtraReads:0,contextExtraRpcs:0,newSsot:false
 };}

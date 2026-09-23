dc5ea60808a0312d26b32d833344bac57f2be764
agridesk <rombouts@agridesk.com>
2026-09-23 13:03:48 +0200

    tests: cover compact decision telemetry and shared context preservation

--- PlanningWorkspaceDecisionReadModelIntegrationTests.js
+++ PlanningWorkspaceDecisionReadModelIntegrationTests.js
@@ -1,15 +1,15 @@
 /***********************************************************************
  * PlanningWorkspaceDecisionReadModelIntegrationTests.js
- * BUILD: 2026-09-23_AMS01_3_WORKSPACE_DECISION_INTEGRATION_R1
+ * BUILD: 2026-09-23_AMS01_3_WORKSPACE_DECISION_INTEGRATION_R2_SHARED_CONTEXT
  ***********************************************************************/
-var PWDRM_INTEGRATION_TEST_BUILD='2026-09-23_AMS01_3_WORKSPACE_DECISION_INTEGRATION_R1';
+var PWDRM_INTEGRATION_TEST_BUILD='2026-09-23_AMS01_3_WORKSPACE_DECISION_INTEGRATION_R2_SHARED_CONTEXT';
 function RUN_AMS01_3_WORKSPACE_DECISION_INTEGRATION_REGRESSION(){
  var svc=String(PlanningWorkspaceService_getDecision),rpc=String(PlanningWorkspaceRpc_bootstrap),r=[];function t(n,v){r.push({name:n,ok:!!v});}
  t('serviceUsesDecisionReadModel',svc.indexOf('PlanningWorkspaceDecisionReadModel_get')>=0);
  t('bootstrapUsesServiceDecision',rpc.indexOf('PlanningWorkspaceService_getDecision(input)')>=0);
  t('bootstrapNoLegacyAdvisoryCall',rpc.indexOf('PlanningWorkspaceService_getAdvisory(input)')<0);
  t('candidateEmailsFromDecisionModel',rpc.indexOf('candidateAuditorEmails')>=0);
- t('sharedAuditContextPreserved',rpc.indexOf('PWR_auditContext_(a)')>=0);
+ t('sharedAuditContextPreserved',rpc.indexOf('a&&a.auditContextById||PWR_auditContext_(a)')>=0);
  t('overlayBoundedPreserved',rpc.indexOf('PWR_overlayInput_(input,e,ctx)')>=0);
  t('compactDecisionTelemetry',rpc.indexOf('compactDecisionModel:true')>=0);
  t('serviceNoSheetAccess',svc.indexOf('SpreadsheetApp')<0&&svc.indexOf('getRange(')<0);

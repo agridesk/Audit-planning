f07fae3c16a3f4c66602a749f4fdfe86832c6b65
agridesk <rombouts@agridesk.com>
2026-09-23 21:29:44 +0200

    fix: remove invalid escaped newline in overview region acceptance

--- AMS03_PlanningOverviewRegionOwnerAcceptance.js
+++ AMS03_PlanningOverviewRegionOwnerAcceptance.js
@@ -5,6 +5,7 @@ function RUN_AMS03_PLANNING_OVERVIEW_REGION_OWNER_ACCEPTANCE(){
  q('companiesIncluded',s.indexOf('includeCompanies:true')>=0);
  q('auditorsExcluded',s.indexOf('includeAuditors:false')>=0);
  q('noDirectCompaniesScan',s.indexOf("getSheetByName('Companies')")<0&&s.indexOf('getDataRange')<0);
- q('companyNameProjectionPreserved',s.indexOf('item.companyName')>=0);\n q('regionProjectionPreserved',s.indexOf('item.region')>=0);
+ q('companyNameProjectionPreserved',s.indexOf('item.companyName')>=0);
+ q('regionProjectionPreserved',s.indexOf('item.region')>=0);
  var f=r.filter(function(x){return!x.ok}).length,o={ok:f===0,build:'2026-09-23_AMS03_PLANNING_OVERVIEW_REGION_OWNER_ACCEPTANCE_R1',total:r.length,passed:r.length-f,failed:f,results:r,meta:{writes:false,newSsot:false}};Logger.log(JSON.stringify(o,null,2));return o;
 }

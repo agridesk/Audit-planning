24705d374a38295c9fd1fdc2b40fd5511c33ab4f
agridesk <rombouts@agridesk.com>
2026-09-24 17:47:14 +0200

    AMS-03 restore canonical Planning Workspace UI include set

--- PlanningWorkspaceUi.js
+++ PlanningWorkspaceUi.js
@@ -1,6 +1,6 @@
 /***********************************************************************
  * PlanningWorkspaceUi.js
- * BUILD: 2026-09-23_ROADMAP_2_4_PLANNING_WORKSPACE_UI_R13_BUILD_SYNC
+ * BUILD: 2026-09-24_ROADMAP_2_4_PLANNING_WORKSPACE_UI_R14_CANONICAL_INCLUDES
  *
  * PERFORMANCE
  * - Route render remains data-independent.
@@ -11,7 +11,7 @@
  * - Modify planning-window context is targeted/on-demand only.
  * - No new cache, no writes during render, no new source of truth.
  ***********************************************************************/
-var PLANNING_WORKSPACE_UI_RENDERER_BUILD='2026-09-23_ROADMAP_2_4_PLANNING_WORKSPACE_UI_R13_BUILD_SYNC';
+var PLANNING_WORKSPACE_UI_RENDERER_BUILD='2026-09-24_ROADMAP_2_4_PLANNING_WORKSPACE_UI_R14_CANONICAL_INCLUDES';
 
 function PlanningWorkspaceUi_render(ctx){
   ctx=ctx||{};
@@ -20,42 +20,46 @@ function PlanningWorkspaceUi_render(ctx){
   t.__seedFrom='';
   t.__seedTo='';
   var html=t.evaluate().getContent();
-  var dragDrop=HtmlService.createHtmlOutputFromFile('PlanningWorkspaceDragDrop.js').getContent();
-  var attentionEnhancer=HtmlService.createHtmlOutputFromFile('PlanningWorkspaceAttentionEnhancer.js').getContent();
-  var pointerDragFallback=HtmlService.createHtmlOutputFromFile('PlanningWorkspacePointerDragFallback.js').getContent();
-  var availabilityContext=HtmlService.createHtmlOutputFromFile('PlanningWorkspaceAvailabilityContext.js').getContent();
-  var detailToolkit=HtmlService.createHtmlOutputFromFile('PlanningWorkspaceDetailToolkit.js').getContent();
-  var plannedActions=HtmlService.createHtmlOutputFromFile('PlanningWorkspacePlannedAuditActions.js').getContent();
-  var modifyWindowGuard=HtmlService.createHtmlOutputFromFile('PlanningWorkspaceModifyWindowGuard.js').getContent();
-  html=html.replace('</body>',dragDrop+'\n'+attentionEnhancer+'\n'+pointerDragFallback+'\n'+availabilityContext+'\n'+detailToolkit+'\n'+plannedActions+'\n'+modifyWindowGuard+'\n</body>');
+  var includeFiles=[
+    'PlanningWorkspaceDragDrop.js',
+    'PlanningWorkspaceContextEnhancer.js',
+    'PlanningWorkspaceAttentionEnhancer.js',
+    'PlanningWorkspacePointerDragFallback.js',
+    'PlanningWorkspaceAvailabilityContext.js',
+    'PlanningWorkspaceDetailToolkit.js',
+    'PlanningWorkspacePlannedAuditActions.js',
+    'PlanningWorkspacePlannedStatusVisuals.js',
+    'PlanningWorkspacePlannedStateDeduper.js',
+    'PlanningWorkspaceAvailabilityLabels.js',
+    'PlanningWorkspaceConceptReview.js',
+    'PlanningWorkspaceUxStabilizer.js',
+    'PlanningWorkspaceModifyWindowGuard.js',
+    'PlanningWorkspaceModifyPickerWeekColumns.js',
+    'PlanningWorkspaceRotationPreassignGuard.js',
+    'PlanningWorkspacePreferredAuditMonths.js'
+  ];
+  var includes=includeFiles.map(function(name){return HtmlService.createHtmlOutputFromFile(name).getContent();}).join('\n');
+  html=html.replace('</body>',includes+'\n</body>');
   return HtmlService.createHtmlOutput(html).setTitle('AMS - Planning Workspace');
 }
 function PlanningWorkspaceUi_contract(){return{
   build:PLANNING_WORKSPACE_UI_RENDERER_BUILD,
   template:'PlanningWorkspace',
   clientInclude:'PlanningWorkspaceClient.js',
-  dragDropInclude:'PlanningWorkspaceDragDrop.js',
+  includeSet:'CANONICAL_R14',
+  contextEnhancerInclude:'PlanningWorkspaceContextEnhancer.js',
   attentionEnhancerInclude:'PlanningWorkspaceAttentionEnhancer.js',
-  pointerDragFallbackInclude:'PlanningWorkspacePointerDragFallback.js',
-  availabilityContextInclude:'PlanningWorkspaceAvailabilityContext.js',
-  detailToolkitInclude:'PlanningWorkspaceDetailToolkit.js',
-  plannedActionsInclude:'PlanningWorkspacePlannedAuditActions.js',
+  plannedStatusVisualsInclude:'PlanningWorkspacePlannedStatusVisuals.js',
+  plannedStateDeduperInclude:'PlanningWorkspacePlannedStateDeduper.js',
+  availabilityLabelsInclude:'PlanningWorkspaceAvailabilityLabels.js',
+  conceptReviewInclude:'PlanningWorkspaceConceptReview.js',
+  uxStabilizerInclude:'PlanningWorkspaceUxStabilizer.js',
   modifyWindowGuardInclude:'PlanningWorkspaceModifyWindowGuard.js',
-  modifyWindowGuardRequired:true,
-  evaluatedTemplate:true,
-  serverSeed:false,
-  dataIndependentShell:true,
-  decisionDataDeferred:true,
-  dragDropClientOnly:true,
-  availabilityContextClientOnly:true,
-  detailToolkitClientOnly:true,
-  plannedActionsClientOnly:true,
-  modifyWindowGuardClientOnly:true,
-  plannedDetailOnDemand:true,
-  plannedActionsRequired:true,
-  planningWindowVisibleOnModify:true,
-  directSheetReads:false,
-  directSheetWrites:false,
-  planningServiceReadsDuringRender:false,
-  newSsot:false
+  modifyPickerWeekColumnsInclude:'PlanningWorkspaceModifyPickerWeekColumns.js',
+  rotationPreassignGuardInclude:'PlanningWorkspaceRotationPreassignGuard.js',
+  preferredAuditMonthsInclude:'PlanningWorkspacePreferredAuditMonths.js',
+  evaluatedTemplate:true,serverSeed:false,dataIndependentShell:true,decisionDataDeferred:true,
+  plannedDetailOnDemand:true,conceptReviewExtraRpcs:0,navigationExtraRpcs:0,
+  rotationCanonicalRevalidationRequired:true,preferredAuditMonthsAdvisoryOnly:true,
+  directSheetReads:false,directSheetWrites:false,planningServiceReadsDuringRender:false,newSsot:false
 };}
